911a85066950553c5be989c08b59fab5f7513d08
Andrew Brown <andrewbrown986@gmail.com>
2020-11-01 21:06:35 -0500

    Working version without semaphores.

--- Reader.cpp
+++ Reader.cpp
@@ -2,31 +2,34 @@
 
 #include <iostream>
 #include "SharedObject.h"
+using namespace std;
 
 struct MyShared{
-	int delay;
-	int threadID;
-	int reportID;
-	bool Running;
+	int sdelay;
+	int sthreadID;
+	int sreportID;
+	bool sRunning;
 };
+
 
 int main(void)
 {
-	std::cout << "I am a reader" << std::endl;
-
-	Shared<MyShared> sharedMem("Shared Memory");//create object of type MyShared named SharedMem  
+	cout << "I am a reader" << endl;
+	Shared<MyShared> sharedMemory("sharedMemory");
+	
 
 	//Reader will poll the shared memory to determine if it is in use, if it is in use
 	// then it will display the data, if it is not it will break 
 	while (true){
-		if (sharedMem->Running == true){
-			std::cout << "Reader Thread: "<<"Thread ID: " <<sharedMem->reportID << ", Report ID: " <<sharedMem->reportID <<" , Delay: "<< sharedMem->delay;
-			sleep(5); 
-		}else
-		{
+		if (sharedMemory->sRunning == true){
+			//std::cout << "Reader Thread: "<<"Thread ID: " <<sharedMemory->sreportID << ", Report ID: " <<sharedMemory->sreportID <<" , Delay: "<< sharedMemory->sdelay;
+			cout << "ThreadID: " << sharedMemory->sthreadID <<" ReportID: "<<sharedMemory->sreportID <<" Delay: " <<sharedMemory->sdelay <<endl;
+			sleep(2); 
+		}else if(sharedMemory->sRunning == false){
 			//When a user enters n the value of Running will be set to false and the loop will break causing the reader to terminate
 			break;
 		}
 	}
-	return 0;
+	//return 0;
 }
+
--- Writer.cpp
+++ Writer.cpp
@@ -13,7 +13,7 @@ struct MyShared{
 };
 
 class WriterThread : public Thread{
-	public:
+public:
 		int 	delay;
 		int 	threadID;
 		int 	reportID = 1;
@@ -45,21 +45,53 @@ class WriterThread : public Thread{
 
 int main(void)
 {
-	std::string userInput;
+	std::string  userInput;
 	std::string userDelay;
-
+	int numThreads=1; //Used for creating writerThread objects where numThreads will be used to give threads ID #s
 
 	std::cout << "I am a Writer" << std::endl;
-	std::cout << "Would you like to create a writer thread?"<< std::endl;
+	
+	WriterThread * thread; //declare thread 
+
+	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
+	//Set the running value to true so the reader can poll the shared memory
+	shared ->sRunning = true;
 
 	
-	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-	
 	while(true){
-		
-	
-		
+		//Prompt user and get input. 
+		std::cout << "Would you like to create a writer thread? Enter Y or N: "<< std::endl;
+		getline(std::cin,userInput);
+		if(userInput == "Y"){
+			std::cout << "Please enter a delay time for this thread: "<< std::endl;
+			getline(std::cin,userDelay);
+			int delay = atoi(userDelay.c_str()); //Convert the string to and int
+
+			//Create a new writerThread object 
+			thread = new WriterThread(delay,numThreads);//instantiate thread 
+			numThreads++; //Increase the number of threads 
+		}
+		else if (userInput == "N"){
+			//If the user enters N then the while statement will break and threads will be handled 
+			if(numThreads -1 !=0){ //Avoids core dump in the case N is entered first and no threads were created
+				shared->sRunning = false;
+				break;
+			}
+			else{
+				break;
+			}
+		}
+		else
+		{
+			//To handle inproper inputs. 
+			std::cout << "Invalid input entered." << std::endl;
+		}
+	}//end while 
+	if(numThreads-1 != 0){ //Avoids core dump in the case N is entered first and no threads were created
+		thread ->flag = true;
+		delete thread; 
 	}
 	
+
 	return 0;	
-}
+}//end main 
